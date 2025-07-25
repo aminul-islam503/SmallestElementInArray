30e26f6610c5b5747b09a03683af93295b8b24b5
aminul-islam503 <aminulis299@gmail.com>
2025-07-25 10:47:02 +0530

    Constructor

---
+++ Constructor.java
@@ -0,0 +1,48 @@
+// public class Constructor {
+//     Constructor(){
+//             System.out.println("CS");
+//             System.out.println("hi from constructor");
+//             System.out.println("CE");
+//     }
+//     public static void main(String[] args) {
+//         System.out.println("MS");
+//         new Constructor();
+//         Constructor c=new Constructor();
+//         System.out.println("ME");
+//     }
+// }
+//
+
+
+
+
+
+
+// TYPE OF CONSTRUCTOR ****1. NO ARG CONSTRUCTOR 2.PARAMETERIZED CONSTRUCTOR
+// public class Constructor {
+//         Constructor(){
+                
+//                 System.out.println("HELLO");
+//         }
+//         public static void main(String[] args) {
+            
+//             Constructor c=new Constructor();
+           
+//         }
+//     }
+    
+
+
+// 2. PARAMETERIZED METHOD
+public class Constructor {
+    Constructor(int a){
+            
+            System.out.println("HELLO");
+    }
+
+    public static void main(String[] args) {
+        
+        Constructor c=new Constructor(2);
+       
+    }
+}
