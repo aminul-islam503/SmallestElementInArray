ab58649731283b5c42745fbe20d4c6b09c0a0afc
aminul-islam503 <aminulis299@gmail.com>
2025-07-27 21:19:29 +0530

    laptop

---
+++ Laptop.java
@@ -0,0 +1,9 @@
+
+// OBJECT IS A BLUE PRINT
+
+public class Laptop {
+    public static void main(String[] args) {
+        System.out.println(new Laptop());
+        System.out.println(new Laptop());
+    }
+}
