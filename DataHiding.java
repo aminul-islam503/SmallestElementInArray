8c4d6042008ee2e302e6947fa2d096318f079f0a
aminul-islam503 <aminulis299@gmail.com>
2025-08-03 22:41:06 +0530

    Data Hiding

---
+++ DataHiding.java
@@ -0,0 +1,12 @@
+public class DataHiding {
+    private double money;
+    public double getmoney()
+    {
+        return money;
+    }
+    public void setmoney(double money)
+    {
+        this.money=money;
+    }
+
+}
